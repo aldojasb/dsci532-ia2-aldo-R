7f91c7471c673830aa44548642f8c49f4fa93530
Aldo S Barros <aldosaltao@gmail.com>
2022-03-11 15:41:13 -0800

    v3

--- app.R
+++ app.R
@@ -7,19 +7,20 @@ library(plotly)
 
 app <- Dash$new(external_stylesheets = dbcThemes$BOOTSTRAP)
 
-penguins <- readr::read_csv('C:/Users/aldos/Documents/LESSONS/532/532-ai2-aldo-R/data/penguins.csv')
-#penguins <- readr::read_csv(here::here('data', 'penguins.csv'))
+#penguins <- readr::read_csv('C:/Users/aldos/Documents/LESSONS/532/532-ai2-aldo-R/data/penguins.csv')
+#head(penguins)
+penguins <- readr::read_csv(here::here('data', 'penguins.csv'))
 
 app$layout(
   dbcContainer(
-    list(htmlH1('Penguin Dashr heroky deployment, DONT select Island and Species. We are still implementing that. Sorry for the inconvenience'),
+    list(htmlH1('Penguin Dashr heroky deployment'),
       dccGraph(id='plot-area'),
       dccDropdown(
         id='col-select',
         options = penguins %>%
           colnames() %>%
           purrr::map(function(col) list(label = col, value = col)), 
-        value='island')
+        value='body_mass_g')
     )
   )
 )
@@ -29,7 +30,7 @@ app$callback(
   list(input('col-select', 'value')),
   function(xcol) {
     p <- ggplot(penguins, aes(x = !!sym(xcol),
-                            y = species,
+                            y = body_mass_g,
                             color = island)) +
       geom_point() +
       scale_x_log10() +
@@ -37,5 +38,5 @@ app$callback(
     ggplotly(p)
   }
 )
-app$run_server(debug = T)
-#app$run_server(host = '0.0.0.0')
+#app$run_server(debug = T)
+app$run_server(host = '0.0.0.0')
